4e8607576518912c0e9de96567dc2e3066e5fa2d
Advait Risbud <advaitris@example.com>
2026-07-24 14:17:02 -0400

    Wrote test to ensure a medium batter should trigger SUN_POINTING mode

--- test.py
+++ test.py
@@ -1,6 +1,16 @@
 from spacecraft import Spacecraft, Mode
 
 def test_starts_in_safe_mode():
-    # Spacecraft should boot in safe mode
+    # Spacecraft should boot in SAFE mode
     sc = Spacecraft()
     assert sc.mode == Mode.SAFE
+
+def test_mid_batt_sun_pointing():
+    # A medium battery should trigger SUN_POINTING mode
+    sc = Spacecraft()
+    sc.battery_lvl = 45
+    sc.in_eclipse = False
+    sc.update_mode()
+    assert sc.mode == Mode.SUN_POINTING
+
+def 
