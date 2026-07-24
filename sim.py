47ba16377bf00cdb23e645c4ad9fa03afd02fea9
Advait Risbud <advaitris@example.com>
2026-07-24 13:50:05 -0400

    Completed simulation code for sim.py.

--- sim.py
+++ sim.py
@@ -1,5 +1,7 @@
 import csv
+import matplotlib.pyplot as plt
 from spacecraft import Spacecraft
+
 
 def run_sim(duration=360, dt=1.0):
     sc = Spacecraft()
@@ -11,3 +13,33 @@ def run_sim(duration=360, dt=1.0):
         t += dt
     return log
 
+def save_csv(log, path="telemtry.csv"):
+    with open(path, "w", newline="") as f:
+        writer = csv.DictWriter(f, fieldnames=log[0].keys())
+        writer.writeheader()
+        writer.writerows(log)
+
+def plot(log):
+    t = [row["t"] for row in log]
+    bat_lvl = [row["battery_lvl"] for row in log]
+    modes = [row["mode"] for row in log]
+
+    fig, ax1 = plt.subplots()
+    ax1.plot(t, bat_lvl, label = "Batter %")
+    ax1.set_xlabel("Time (min)")
+    ax1.set_ylabel("Battery %")
+
+    for i in range(len(t) - 1):
+        color = {"SAFE": "red", "SUN_POINTING": "yellow", "NOMINAL": "green"}[modes[i]]
+        ax1.axvspan(t[i], t[i+1], color = color, alpha = 0.1)
+
+    plt.title("Spacecraft Mode & Battery Over Time")
+    plt.tight_layout()
+    plt.savefig("sim_output.png")
+    plt.show()
+
+if __name__ == "__main__":
+    log = run_sim()
+    save_csv()
+    plot(log)
+    print(f"Simulation complete. {len(log)} steps have been logged to telemetry.csv")
